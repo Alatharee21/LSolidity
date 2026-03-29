da7f5618cb20690604022db1af46415627133f09
Rasheed <hidrahtweet@gmail.com>
2026-03-29 22:50:13 +0100

    documentation helps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,4 +6,11 @@ export default defineConfig({
   solidity: {
     version: "0.8.28",
   },
+  networks: {
+    sepolia: {
+      type: "http",
+      url: "http://rpc.sepolia.org",
+      accounts: ["59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"],
+    },
+  },
 });
